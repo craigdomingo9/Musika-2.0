cdf80103a1882ed695b1ff89c2141761555cba8b
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-10-26 03:38:12 +0200

    communications batch fix

--- app/backend/apps/communications/admin.py
+++ app/backend/apps/communications/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Conversation,Participant,Message
 # Register your models here.
+
+
+admin.site.register(Conversation)
+admin.site.register(Participant)
+admin.site.register(Message)
