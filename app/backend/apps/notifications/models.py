88bb7ddeaa79318a80eada687fe1dfaed92582f0
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-10-27 02:21:13 +0200

    notifications batch fix

--- app/backend/apps/notifications/models.py
+++ app/backend/apps/notifications/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class Notification(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    message = models.CharField(max_length=255)
+    message = models.CharField(max_length=1000)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
