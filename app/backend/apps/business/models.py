8d07333c3f373f8a972492a9f1e2fc683666d13e
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-10-30 02:38:27 +0200

    business batch fix

--- app/backend/apps/business/models.py
+++ app/backend/apps/business/models.py
@@ -147,7 +147,7 @@ class ProductImage(models.Model):
 
 """   Product Variant   """
 class ProductVariant(models.Model):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="images")
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="variant")
     stock_quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2,blank=True )
     on_sale = models.BooleanField(default=False)
