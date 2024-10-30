36ccc1f28a54e64f3f0bab29753690a39860dbe2
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-10-30 02:37:49 +0200

    payment batch

--- app/backend/apps/payment/models.py
+++ app/backend/apps/payment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Customer making the purchase
+    agent_code = models.CharField(max_length=100, blank=True, null=True)  # Agent code
+    business_code = models.CharField(max_length=100, blank=True, null=True)  # Agent code
+    purchase_date = models.DateTimeField(auto_now_add=True)  # When the purchase was made
+    product = models.CharField(max_length=100)  # Product purchased
+    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Amount of purchase
+
