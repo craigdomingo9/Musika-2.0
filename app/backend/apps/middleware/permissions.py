900af59986d2a6225f9312ea4167f9e1b7785efa
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-11-01 12:42:36 +0200

    integrated custom permissions into middleware

---
+++ app/backend/apps/middleware/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+
+class IsAccountOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        print(obj.user.uuid, request.user.uuid)
+        return obj.user == request.user
+
+
+class IsBusinessOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of a business to access related resources.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is associated with the business of the object
+        return obj.business.user == request.user  # Assuming each object has a related business with an owner
+
+class IsAgentOrBusiness(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of a business to access related resources.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is associated with the business of the object
+        return obj.business_offer.business.user.is_agent == True | obj.business_offer.business.user.is_business == True   # Assuming each object has a related business with an owner
