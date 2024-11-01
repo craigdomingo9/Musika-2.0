9e31ba9adef47897647427657e52fb09cb403922
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-11-01 04:59:46 +0200

    removed redundant import

--- app/backend/apps/communications/views.py
+++ app/backend/apps/communications/views.py
@@ -1,5 +1,4 @@
 # views.py
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework import viewsets, status
