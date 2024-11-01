9691a5c2ad26abc467a7faf1d4aad816d12432ef
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-11-01 15:03:18 +0200

    added is_read filter

--- app/backend/apps/notifications/filters.py
+++ app/backend/apps/notifications/filters.py
@@ -3,7 +3,8 @@ from .models import Notification
 
 class NotificationFilter(django_filters.FilterSet):
     user_uuid = django_filters.UUIDFilter(field_name='user__uuid', lookup_expr='exact')
+    is_read = django_filters.BooleanFilter(field_name='is_read')
     
     class Meta:
         model = Notification
-        fields = ['user_uuid']
+        fields = ['user_uuid', 'is_read']
