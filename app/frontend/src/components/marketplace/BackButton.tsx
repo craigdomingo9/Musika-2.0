46c8140e3de383b3c2d78a6aa9a1245d1b0a3684
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-12-08 12:11:50 +0200

    Added boldness to the back button text

--- app/frontend/src/components/marketplace/BackButton.tsx
+++ app/frontend/src/components/marketplace/BackButton.tsx
@@ -8,7 +8,7 @@ function BackButton() {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 py-auto">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
       </svg>
-      <p>{backButtonName}</p>
+      <p className="font-semibold text-opacity">{backButtonName}</p>
     </Link>
   )
 }
