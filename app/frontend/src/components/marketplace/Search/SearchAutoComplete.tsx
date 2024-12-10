563cdbc6eab4a0890c78c06e420f4021982f41e2
Craig Kiki Domingo <craigdomingo9@gmail.com>
2024-12-11 01:38:44 +0200

    Added submit on enter functionality to search input

--- app/frontend/src/components/marketplace/Search/SearchAutoComplete.tsx
+++ app/frontend/src/components/marketplace/Search/SearchAutoComplete.tsx
@@ -32,8 +32,8 @@ function SearchAutoComplete({searchQuery}: Props) {
   return (
     <>
     {searchQuery && (
-      <div className="absolute flex flex-col z-50 bg-white w-[19rem] px-4 mt-3 min-h-96 shadow-lg">
-        {searchResults.map((result) => (
+      <div className="absolute flex flex-col z-50 bg-white w-[16.5rem] sm:w-[19rem] px-4 mt-3 min-h-96 shadow-xl rounded-lg">
+        {searchResults.length > 0 ? searchResults.map((result) => (
           <Link href={{
             pathname: "/product",
             query: {
@@ -44,7 +44,11 @@ function SearchAutoComplete({searchQuery}: Props) {
           className="h-10">
             {result.name}
           </Link>
-        ))}
+        )) : (
+          <div className="min-h-full grid items-center">
+            <p className="m-auto pt-4">Nothing found</p>
+          </div>
+        )}
       </div>
     )}
     </>
